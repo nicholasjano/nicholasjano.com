b45ef5e0e0e88918b70785ac6c27c9f65fbbff87
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-06 17:03:53 -0500

    Created some base ccs attributes, and added in colours, and various setup changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-background": "#141414",
+        "secondary-background": "#171819",
+        header: "rgb(16 17 18 / 0.72)",
+        "header-stroke": "rgb(224 238 252 / 0.15)",
+        "main-green": "#19B345",
+        "main-blue": "#4FB4E6",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};

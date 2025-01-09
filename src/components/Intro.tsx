1ebe5a00735554fb70cd5e42527e297510f508b8
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-09 03:05:17 -0500

    Moved Symbols

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -13,7 +13,7 @@ const Intro = () => {
     >
       <IntroSymbols x={x} y={y} />
 
-      <div className="z-10 flex flex-col items-center justify-center space-y-2">
+      <div className="z-10 flex flex-col items-center justify-center text-center space-y-2">
         <h1 className="text-7xl font-bold bg-gradient-to-r from-brand-green from-20% via-brand-blue via-50% to-brand-green to-80% text-transparent bg-clip-text bg-400% animate-gradient">
           Nicholas Jano
         </h1>

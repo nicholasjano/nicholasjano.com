4fffd52bc06c33a83b782f81a796dc7c3f16e991
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-09 02:03:02 -0500

    Moved Symbols

--- src/data/pageData.ts
+++ src/data/pageData.ts
@@ -1,8 +1,8 @@
 export const symbols = [
   { text: "{}", x: 1, y: 1, horizontal: "5%", vertical: "12%" },
   { text: "[]", x: 1, y: 1, horizontal: "20%", vertical: "45%" },
-  { text: "++", x: 1, y: 1, horizontal: "81%", vertical: "60%" },
-  { text: "</>", x: 1, y: -1, horizontal: "85%", vertical: "80%" },
+  { text: "++", x: 1, y: 1, horizontal: "79%", vertical: "60%" },
+  { text: "</>", x: 1, y: -1, horizontal: "86%", vertical: "80%" },
   { text: "||", x: 1, y: -1, horizontal: "65%", vertical: "20%" },
   { text: "!==", x: 1, y: -1, horizontal: "20%", vertical: "15%" },
   { text: "= >", x: -1, y: 1, horizontal: "59%", vertical: "82%" },

07ac3b960090e8788114db828ec185ab154e0a97
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-14 04:06:17 -0500

    Changed Symbol

--- src/data/pageData.ts
+++ src/data/pageData.ts
@@ -1,19 +1,34 @@
 export const generateSymbols = (radius: number) => {
   const baseSymbols = [
-    "{}", "++", "</>", "!==", "//", "[]", "==", ":=", "**", "&&", ">>", "->"
+    "{}",
+    "++",
+    "</>",
+    "!==",
+    "//",
+    "[]",
+    "==",
+    ":=",
+    "**",
+    "||",
+    ">>",
+    "->",
   ];
   const numSymbols = baseSymbols.length;
   const symbols = [];
 
   for (let i = 0; i < numSymbols; i++) {
-    const angle = (Math.PI / 2) - (i * (2 * Math.PI / numSymbols)); 
+    const angle = Math.PI / 2 - i * ((2 * Math.PI) / numSymbols);
     let x = Math.cos(angle) * radius + 50;
     let y = -Math.sin(angle) * radius + 50;
 
     x = Math.round(x * 1000) / 1000;
     y = Math.round(y * 1000) / 1000;
 
-    symbols.push({ text: baseSymbols[i], horizontal: `${x}%`, vertical: `${y}%` });
+    symbols.push({
+      text: baseSymbols[i],
+      horizontal: `${x}%`,
+      vertical: `${y}%`,
+    });
   }
 
   return symbols;

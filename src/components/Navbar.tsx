1442164d0c81039944e1a039e804a35ae7ce7549
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-18 15:39:00 -0500

    Updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full h-12 border-b border-[rgba(224,238,252,0.15)] bg-[rgba(16,17,18,0.72)] backdrop-blur-[30px] z-50"></nav>
+    <nav className="fixed top-0 left-0 w-full h-12 border-b border-header-stroke bg-header-primary backdrop-blur-[20px] z-50"></nav>
   );
 };
 

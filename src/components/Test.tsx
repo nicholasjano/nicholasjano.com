ef8b8cc95f96726e38cd09c9bad9d4366032475f
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-20 23:44:08 -0500

    Dynamic test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,7 @@
 const Test = () => {
   return (
     <div className="overflow-x-clip absolute inset-x-0 top-0">
-      <div className="absolute inset-0 w-screen h-screen flex items-center justify-center overflow-hidden bg-black transform translate-y-full z-30"></div>
+      <div className="absolute inset-0 w-screen min-h-screen flex items-center justify-center overflow-hidden bg-black transform translate-y-full z-30"></div>
     </div>
   );
 };

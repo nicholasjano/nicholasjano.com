bb6161425bfb19364805ba256416595a849d9118
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-25 15:06:16 -0500

    Fixed navbar gltch on load

--- src/hooks/useScreenSize.ts
+++ src/hooks/useScreenSize.ts
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
 
 export const useScreenSize = (minWidth = 768, minHeight = 480) => {
-  const [isLargeScreen, setIsLargeScreen] = useState(false);
+  const getIsLargeScreen = () =>
+    window.innerWidth >= minWidth && window.innerHeight >= minHeight;
+
+  // Initialize with the current screen size
+  const [isLargeScreen, setIsLargeScreen] = useState(getIsLargeScreen);
 
   useEffect(() => {
     const checkSize = () => {
-      setIsLargeScreen(
-        window.innerWidth >= minWidth && window.innerHeight >= minHeight
-      );
+      setIsLargeScreen(getIsLargeScreen());
     };
-
-    // Initial check
-    checkSize();
 
     // Add event listener
     window.addEventListener("resize", checkSize);

fb1ae781f272263933eae2e25fb1df25d10924af
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-26 04:18:30 -0500

    Updated Stack responsivness

--- src/components/StackCard.tsx
+++ src/components/StackCard.tsx
@@ -2,8 +2,8 @@ import type { StackListProps } from "@pageTypes/pageTypes";
 
 const StackCard = ({ title, items }: StackListProps) => {
   return (
-    <div className="bg-surface-secondary p-4 rounded-lg space-y-4 flex flex-col items-center xs:items-start">
-      <h4 className="stack-title">{title}</h4>
+    <div className="bg-surface-secondary p-4 rounded-lg space-y-4 flex flex-col">
+      <h4 className="stack-title mx-auto text-center xs:mx-0 xs:text-left">{title}</h4>
       <ul className="flex flex-col gap-4">
         {items.map((item) => (
           <li key={item.name} className="h-12 flex items-center gap-3">

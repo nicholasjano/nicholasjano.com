51c0e65d4aa01cdcc622f6884f450b669c110225
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-28 22:55:54 -0500

    Update to make the date update properly

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,13 +1,15 @@
 import AnimatedNumber from "@components/AnimatedNumber";
+import { getMonthlyDates } from "@utilities/GetDateEST"
 
 const Stats = () => {
+  const { prevMonth, currMonth } = getMonthlyDates();
   return (
     <section id="stats" className="overflow-x-clip absolute inset-x-0 top-0">
       <div className="absolute inset-0 w-screen h-screen overflow-hidden bg-surface-dark transform translate-y-full z-30">
         <div className="h-full w-full flex flex-col justify-center items-center space-y-4 stats-xs:space-y-20">
           <div className="flex flex-col justify-center items-center text-center space-y-2">
             <h2>My Monthly Stats</h2>
-            <p className="under-header">12/21/24 - 01/21/25</p>
+            <p className="under-header">{prevMonth} - {currMonth}</p>
           </div>
           <div className="w-fit stats-cols:width-scaled max-w-screen-2xl grid grid-rows-3 stats-cols:grid-rows-none stats-cols:grid-cols-3 gap-2 stats-xs:gap-4 text-center text-base sm:text-lg font-normal mx-12 stats-cols:mx-0">
             <div className="stats-info">

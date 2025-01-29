00ab93cef1c3426a7661374c971da1669284d81f
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-01-28 22:23:45 -0500

    Fixed expeirence dropdown bug

--- src/hooks/useResizeHeight.ts
+++ src/hooks/useResizeHeight.ts
@@ -14,15 +14,24 @@ export const useResizeHeight = ({
     const updateHeight = (): void => {
       if (contentRef.current) {
         setIsResizing(true);
-        setMaxHeight(contentRef.current.scrollHeight);
+        const height = contentRef.current.scrollHeight + 2;
+        setMaxHeight(height);
         setTimeout(() => setIsResizing(false), 50);
       }
     };
 
+    // Initial measurement
     updateHeight();
+    // Secondary measurement to catch any late layout changes
+    const initialTimeout = setTimeout(updateHeight, 50);
+
+    // Update on window resize
     window.addEventListener("resize", updateHeight);
 
-    return (): void => window.removeEventListener("resize", updateHeight);
+    return (): void => {
+      window.removeEventListener("resize", updateHeight);
+      clearTimeout(initialTimeout);
+    };
   }, [contentRef]);
 
   return { maxHeight, isResizing };
